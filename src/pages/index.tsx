0a8f87d47d5d9dfb0fd060e0a21d39e35c586f7b
Guillermo Perez <gperez78@gmail.com>
2023-04-16 22:40:56 -0300

    Preload icon images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,15 @@ export default function Home() {
         <meta name="description" content="Collaborative patchwork" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
+        <link rel="preload" href="/default.png" as="image" />
+        <link rel="preload" href="/pointer.png" as="image" />
+        <link rel="preload" href="/draw.png" as="image" />
+        <link rel="preload" href="/paint.png" as="image" />
+        <link rel="preload" href="/move.png" as="image" />
+        <link rel="preload" href="/rotate.png" as="image" />
+        <link rel="preload" href="/delete.png" as="image" />
+        <link rel="preload" href="/forbiden.png" as="image" />
       </Head>
       <App />
     </>
